2b18bc60b7f46bcef68f409b7b741a12add81cfc
Ferdinand Keller <ferdinand.keller@proton.me>
2024-08-02 09:13:37 +0000

    created decompilation script

---
+++ mc-source-code/decompile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script decompiles the official Minecraft server JAR file.
+# Be careful, as releasing the decompiled source code is against Minecraft's EULA.
+
+# configure which version we want to decompile
+MC_VERSION="1.21"
+
+# clone DecompilerMC for automatic decompilation
+# look here for more informations:
+# - https://minecraft.fandom.com/wiki/Tutorials/See_Minecraft%27s_code
+# - https://github.com/hube12/DecompilerMC
+git clone https://github.com/hube12/DecompilerMC.git
+cd DecompilerMC
+python3.12 main.py -mcv $MC_VERSION -s server -na -f -rmap -rjar -dm -dj -dd -dec -q -c
+mv src ..
