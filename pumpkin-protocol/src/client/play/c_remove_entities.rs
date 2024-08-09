e96f072951652f029d4aaaa8fe08ab99c23701b7
Snowiiii <lilalexmed@proton.me>
2024-08-09 12:52:53 +0200

    Fix: Clientbound remove entities

--- pumpkin-protocol/src/client/play/c_remove_entities.rs
+++ pumpkin-protocol/src/client/play/c_remove_entities.rs
@@ -6,11 +6,12 @@ use crate::VarInt;
 #[derive(Serialize, Clone)]
 #[packet(0x42)]
 pub struct CRemoveEntities {
+    count: VarInt,
     entitiy_ids: Vec<VarInt>,
 }
 
 impl CRemoveEntities {
     pub fn new(entitiy_ids: Vec<VarInt>) -> Self {
-        Self { entitiy_ids }
+        Self { count: VarInt(entitiy_ids.len() as i32), entitiy_ids }
     }
 }
