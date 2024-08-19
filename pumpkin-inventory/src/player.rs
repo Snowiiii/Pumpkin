9223c8bf5419d44a1349b350e166a6b9970f1b3e
Edvin Bryntesson <epost@edvinbryntesson.se>
2024-08-19 23:35:45 +0200

    Add missing slots in inventory

--- pumpkin-inventory/src/player.rs
+++ pumpkin-inventory/src/player.rs
@@ -2,12 +2,26 @@ use pumpkin_world::item::Item;
 
 pub struct PlayerInventory {
     // Main Inventory + Hotbar
+    crafting: [Option<Item>; 4],
+    crafting_output: Option<Item>,
     items: [Option<Item>; 36],
     armor: [Option<Item>; 4],
     offhand: Option<Item>,
-    // current selected slot in hortbar
-    selected: i16,
+    // current selected slot in hotbar
+    selected: usize,
 }
+
+pub struct Hotbar<'a>(&'a mut [Option<Item>;9]);
+
+impl Hotbar<'_> {
+    fn get_mut(&mut self, index: usize) -> &mut Option<Item> {
+        &mut self.0[index]
+    }
+}
+
+pub struct Armor<'a>(&'a mut [Option<Item>; 4]);
+
+
 
 impl Default for PlayerInventory {
     fn default() -> Self {
@@ -18,6 +32,8 @@ impl Default for PlayerInventory {
 impl PlayerInventory {
     pub fn new() -> Self {
         Self {
+            crafting: [None; 4],
+            crafting_output: None,
             items: [None; 36],
             armor: [None; 4],
             offhand: None,
