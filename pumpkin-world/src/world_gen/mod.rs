9149e82508515bbfc56e005bfef1a97f68d597f5
DaniD3v <sch220233@spengergasse.at>
2024-08-28 10:49:05 +0200

    Added WorldGenerator traits and a GenericWorldGenerator.

--- pumpkin-world/src/world_gen/mod.rs
+++ pumpkin-world/src/world_gen/mod.rs
@@ -1,1 +1,11 @@
+mod generator;
+mod generic_generator;
 mod seed;
+
+pub use generator::WorldGenerator;
+pub use seed::Seed;
+
+#[allow(dead_code)]
+pub fn get_world_gen(_: Seed) -> Box<dyn WorldGenerator> {
+    todo!()
+}
