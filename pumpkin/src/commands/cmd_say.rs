b3561a55247a16aa5a4aed7aaba0a8b15c4bb916
van-sprundel <ramonvansprundel@gmail.com>
2024-10-18 22:32:38 +0200

    Update impl

--- pumpkin/src/commands/cmd_say.rs
+++ pumpkin/src/commands/cmd_say.rs
@@ -17,13 +17,14 @@ pub fn init_command_tree<'a>() -> CommandTree<'a> {
     CommandTree::new(NAMES, DESCRIPTION).with_child(
         argument(ARG_CONTENT, consume_arg_content).execute(&|sender, server, args| {
             if let Some(content) = args.get("content") {
-                let message = &format!("[Server]: {content}");
+                let message = &format!("[Console]: {content}");
+                let message = TextComponent::text(message).color_named(NamedColor::Blue);
 
-                server
-                    .broadcast_message(&TextComponent::text(message).color_named(NamedColor::Blue));
+                server.broadcast_message(message.clone());
+                sender.send_message(message);
             } else {
                 sender.send_message(
-                    TextComponent::text("Please provide a message: /say [content]")
+                    TextComponent::text("Please provide a message: say [content]")
                         .color_named(NamedColor::Red),
                 );
             }
