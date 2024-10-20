bcd7a44461ff8af71ae8179a909f9ba1d979cd1b
Hongas <ndhiqvocuc@gmail.com>
2024-10-20 18:07:51 +0700

    Use Translations Fixes #120

--- pumpkin/src/server/translation.rs
+++ pumpkin/src/server/translation.rs
@@ -1,3 +1,5 @@
+#![expect(dead_code)]
+
 use std::{
     collections::HashMap,
     fs::File,
@@ -19,20 +21,23 @@ pub fn translate(
     message: &str,
 ) -> Result<HashMap<String, String>, TranslationError> {
     let file = File::open(path.into()).map_err(|_| TranslationError::InvalidFile)?;
-    let translations = fetch_translations(&file, message)?;
+    let mut bufreader = BufReader::new(file);
+    let translations = fetch_translations(&mut bufreader, message)?;
     let results = make_hashmap(translations);
 
     Ok(results)
 }
 
 //Read a huge object line by line and tricking serde_json into thinking they are individual objects
-fn fetch_translations(file: &File, message: &str) -> Result<Vec<Value>, TranslationError> {
-    let mut bufreader = BufReader::new(file);
+fn fetch_translations(
+    mut reader: impl BufRead,
+    message: &str,
+) -> Result<Vec<Value>, TranslationError> {
     let mut buf = String::new();
     let mut results = Vec::new();
 
     loop {
-        let bytes_read = bufreader
+        let bytes_read = reader
             .read_line(&mut buf)
             .map_err(|_| TranslationError::FileRead)?;
 
@@ -59,15 +64,17 @@ fn fetch_translations(file: &File, message: &str) -> Result<Vec<Value>, Translat
 
 fn make_hashmap(vec: Vec<Value>) -> HashMap<String, String> {
     let mut hashmap: HashMap<String, String> = HashMap::new();
+    let empty_string = String::default(); //Compiler complains about temporary values, clippy complains about unwrap_or
+
     for value in vec {
         if let Value::Object(map) = value {
-            let text = map.keys().next().unwrap_or(&String::default()).to_owned();
+            let text = map.keys().next().unwrap_or(&empty_string);
             if let Value::String(translation) = map
                 .values()
                 .next()
                 .unwrap_or(&Value::String(String::default()))
             {
-                hashmap.insert(text, translation.to_owned());
+                hashmap.insert(text.to_owned(), translation.to_owned());
             }
         }
     }
@@ -79,11 +86,50 @@ fn make_hashmap(vec: Vec<Value>) -> HashMap<String, String> {
 mod test {
     use std::collections::HashMap;
 
-    use super::translate;
+    use super::{fetch_translations, make_hashmap};
+
+    #[test]
+    fn test_lang_ja_jp() {
+        let reader = std::io::Cursor::new(
+            r#"
+            {
+            "advancement.advancementNotFound": "\u4e0d\u660e\u306a\u9032\u6357\u3067\u3059\uff1a%s",
+            "advancements.adventure.bullseye.description": "30m\u4ee5\u4e0a\u96e2\u308c\u305f\u5834\u6240\u304b\u3089\u7684\u306e\u4e2d\u5fc3\u3092\u5c04\u629c\u304f",
+            "advancements.adventure.bullseye.title": "\u7684\u4e2d",
+            "commands.advancement.advancementNotFound": "\u300c%s\u300d\u3068\u3044\u3046\u540d\u524d\u306e\u9032\u6357\u306f\u898b\u3064\u304b\u308a\u307e\u305b\u3093\u3067\u3057\u305f"
+        }"#.as_bytes()
+        );
+
+        let intended_result = HashMap::from([
+            (
+                "commands.advancement.advancementNotFound".to_owned(),
+                "「%s」という名前の進捗は見つかりませんでした".to_owned(),
+            ),
+            (
+                "advancement.advancementNotFound".to_owned(),
+                "\u{4e0d}\u{660e}\u{306a}\u{9032}\u{6357}\u{3067}\u{3059}\u{ff1a}%s".to_owned(),
+            ),
+        ]);
+
+        assert_eq!(
+            intended_result,
+            make_hashmap(fetch_translations(reader, "advancement.advancementNotFound").unwrap())
+        );
+    }
 
     #[test]
     fn test_lang_it_it() {
-        let path = "C:/Users/ADMIN/AppData/Roaming/.minecraft/assets/objects/67/6793f17e4febc8801da3877f2a058c1ef77c338a";
+        let reader = std::io::Cursor::new(
+            r#"
+            {
+                "advMode.type": "Tipo",
+                "advancement.advancementNotFound": "Progresso sconosciuto: %s",
+                "advancements.adventure.adventuring_time.description": "Scopri tutti i biomi",
+                "advancements.adventure.adventuring_time.title": "All'avventura!"
+        }"#
+            .as_bytes(),
+        );
+
         let intended_result = HashMap::from([(
             "advancement.advancementNotFound".to_owned(),
             "Progresso sconosciuto: %s".to_owned(),
@@ -91,27 +137,27 @@ mod test {
 
         assert_eq!(
             intended_result,
-            translate(path, "advancement.advancementNotFound").unwrap()
+            make_hashmap(fetch_translations(reader, "advancement.advancementNotFound").unwrap())
         );
     }
 
     #[test]
-    fn test_lang_ja_jp() {
-        let path = "C:/Users/ADMIN/AppData/Roaming/.minecraft/assets/objects/09/09dcd0a0f7313920d433da26b26d9bb451f24594";
-        let intended_result = HashMap::from([
-            (
-                "advancement.advancementNotFound".to_owned(),
-                "\u{4e0d}\u{660e}\u{306a}\u{9032}\u{6357}\u{3067}\u{3059}\u{ff1a}%s".to_owned(),
-            ),
-            (
-                "commands.advancement.advancementNotFound".to_owned(),
-                "「%s」という名前の進捗は見つかりませんでした".to_owned(),
-            ),
-        ]);
+    fn no_match() {
+        let reader = std::io::Cursor::new(
+            r#"
+            {
+                "accessibility.onboarding.accessibility.button": "Accessibilità...",
+                "accessibility.onboarding.screen.narrator": "Premi Invio per attivare l'assistente vocale",
+                "accessibility.onboarding.screen.title": "Ti diamo il benvenuto in Minecraft!\n\nVuoi attivare l'assistente vocale o accedere alle impostazioni di accessibilità?",
+                "addServer.add": "Fatto"
+        }"#.as_bytes()
+        );
+
+        let intended_result = HashMap::new();
 
         assert_eq!(
             intended_result,
-            translate(path, "advancement.advancementNotFound").unwrap()
+            make_hashmap(fetch_translations(reader, "advancement.advancementNotFound").unwrap())
         );
     }
 }
