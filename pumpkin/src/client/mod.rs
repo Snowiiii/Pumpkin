2afe4c5d4efd96c80b548d551254d2e0bbfc1e6f
lukas0008 <70709598+lukas0008@users.noreply.github.com>
2024-10-21 18:01:15 +0200

    Fix mistake in merging

--- pumpkin/src/client/mod.rs
+++ pumpkin/src/client/mod.rs
@@ -211,7 +211,7 @@ impl Client {
     pub async fn process_packets(&self, server: &Arc<Server>) {
         while let Some(mut packet) = self.client_packets_queue.lock().await.pop_front() {
             if let Err(error) = self.handle_packet(server, &mut packet).await {
-                log::error!("Error while reading incoming packet {error}", text);
+                log::error!("Error while reading incoming packet {error}");
                 self.kick(&text).await;
             };
         }
