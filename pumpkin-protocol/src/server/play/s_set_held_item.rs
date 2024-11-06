a1926ffea6b324d58132e0c1d9c58873b0bd98e3
Gyula Kiri <duszmox@gmail.com>
2024-11-06 20:24:40 +0100

    Fix: Use correct packet for set_help_item (#235)
    * According to the wiki we need to use 0x2F for set held item, this fixes the bug where if you have something in your 1st slot you would be able to place it with any other slot selected

--- pumpkin-protocol/src/server/play/s_set_held_item.rs
+++ pumpkin-protocol/src/server/play/s_set_held_item.rs
@@ -2,7 +2,7 @@ use pumpkin_macros::server_packet;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-#[server_packet("play:pick_item")]
+#[server_packet("play:set_carried_item")]
 pub struct SSetHeldItem {
     pub slot: i16,
 }
