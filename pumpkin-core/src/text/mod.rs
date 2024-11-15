7b64ca10c358aa784b7f0eda25cb9f152f3a8a9b
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-11-15 22:39:52 +0100

    Term hyperlinks (#290)
    * Add hyperlinks to console (see OSC 8 ANSI escape sequence)

--- pumpkin-core/src/text/mod.rs
+++ pumpkin-core/src/text/mod.rs
@@ -76,6 +76,12 @@ impl<'a> TextComponent<'a> {
         if style.strikethrough.is_some() {
             text = text.strikethrough().to_string();
         }
+        if style.click_event.is_some() {
+            if let Some(ClickEvent::OpenUrl(url)) = style.click_event {
+                //TODO: check if term supports hyperlinks before
+                text = format!("\x1b]8;;{}\x1b\\{}\x1b]8;;\x1b\\", url, text).to_string()
+            }
+        }
         text
     }
 }
