10f3b4f3d28770b5ba5b1e6fcb65fa2898214d24
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-11-17 10:26:05 +0100

    Update port comment in pumpkin-config/src/lan_broadcast.rs
    Co-authored-by: we sell insurance <72574589+neeleshpoli@users.noreply.github.com>

--- pumpkin-config/src/lan_broadcast.rs
+++ pumpkin-config/src/lan_broadcast.rs
@@ -9,7 +9,9 @@ pub struct LANBroadcastConfig {
     // We use an extra motd because this only supports one line
     #[serde_inline_default("A Blazing fast Pumpkin Server!".to_string())]
     pub motd: String,
-    // No idea why you would want to change this
+    // Allow users to specify port so the port is predictable
+    // There are many reasons why the port might need to be predictable
+    // One reason is docker containers, where specific ports need to be allowed
     #[serde_inline_default(None)]
     pub port: Option<u16>,
 }
