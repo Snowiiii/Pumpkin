230a99adc511c71debf198bf85a15b7deaf93d94
Snowiiii <lilalexmed@proton.me>
2024-11-17 22:02:33 +0100

    Fix: Extractor Packet build

--- extractor/src/main/kotlin/de/snowii/extractor/extractors/Packets.kt
+++ extractor/src/main/kotlin/de/snowii/extractor/extractors/Packets.kt
@@ -47,33 +47,6 @@ class Packets : Extractor.Extractor {
 
 
     private fun serializeServerBound(
-        packets: Array<NetworkState.Factory<out ClientPacketListener, out PacketByteBuf>>
-    ): JsonObject {
-        val statusArray = JsonObject()
-        val loginArray = JsonObject()
-        val configArray = JsonObject()
-        val playArray = JsonObject()
-
-        for (factory in packets) {
-            factory.forEachPacketType { type: PacketType<*>, i: Int ->
-                when (factory.phase()!!) {
-                    NetworkPhase.HANDSHAKING -> error("Server bound Packet should have no handshake")
-                    NetworkPhase.PLAY -> playArray.addProperty(type.id().path, i)
-                    NetworkPhase.STATUS -> statusArray.addProperty(type.id().path, i)
-                    NetworkPhase.LOGIN -> loginArray.addProperty(type.id().path, i)
-                    NetworkPhase.CONFIGURATION -> configArray.addProperty(type.id().path, i)
-                }
-            }
-        }
-        val finalJson = JsonObject()
-        finalJson.add("status", statusArray)
-        finalJson.add("login", loginArray)
-        finalJson.add("config", configArray)
-        finalJson.add("play", playArray)
-        return finalJson
-    }
-
-    private fun serializeClientBound(
         packets: Array<NetworkState.Factory<out ServerCrashSafePacketListener, out ByteBuf>>
     ): JsonObject {
         val handshakeArray = JsonObject()
@@ -95,6 +68,33 @@ class Packets : Extractor.Extractor {
         }
         val finalJson = JsonObject()
         finalJson.add("handshake", handshakeArray)
+        finalJson.add("status", statusArray)
+        finalJson.add("login", loginArray)
+        finalJson.add("config", configArray)
+        finalJson.add("play", playArray)
+        return finalJson
+    }
+
+    private fun serializeClientBound(
+        packets: Array<NetworkState.Factory<out ClientPacketListener, out PacketByteBuf>>
+    ): JsonObject {
+        val statusArray = JsonObject()
+        val loginArray = JsonObject()
+        val configArray = JsonObject()
+        val playArray = JsonObject()
+
+        for (factory in packets) {
+            factory.forEachPacketType { type: PacketType<*>, i: Int ->
+                when (factory.phase()!!) {
+                    NetworkPhase.HANDSHAKING -> error("Client bound Packet should have no handshake")
+                    NetworkPhase.PLAY -> playArray.addProperty(type.id().path, i)
+                    NetworkPhase.STATUS -> statusArray.addProperty(type.id().path, i)
+                    NetworkPhase.LOGIN -> loginArray.addProperty(type.id().path, i)
+                    NetworkPhase.CONFIGURATION -> configArray.addProperty(type.id().path, i)
+                }
+            }
+        }
+        val finalJson = JsonObject()
         finalJson.add("status", statusArray)
         finalJson.add("login", loginArray)
         finalJson.add("config", configArray)
