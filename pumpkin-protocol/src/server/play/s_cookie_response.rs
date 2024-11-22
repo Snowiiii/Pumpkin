9da1ef388119de7dd56823768f22a07ecf4ca9a9
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-11-22 11:36:30 +0100

    Update pumpkin-protocol/src/server/play/s_cookie_response.rs
    Co-authored-by: DataModel <183248792+DataM0del@users.noreply.github.com>

--- pumpkin-protocol/src/server/play/s_cookie_response.rs
+++ pumpkin-protocol/src/server/play/s_cookie_response.rs
@@ -4,7 +4,7 @@ use crate::{Identifier, VarInt};
 #[derive(serde::Deserialize, Debug)]
 #[server_packet("play:cookie_response")]
 /// Response to a Cookie Request (play) from the server.
-/// The Notchian server only accepts responses of up to 5 kiB in size.
+/// The Notchian (vanilla) server only accepts responses of up to 5 kiB in size.
 pub struct SCookieResponse {
     pub key: Identifier,
     pub has_payload: bool,
