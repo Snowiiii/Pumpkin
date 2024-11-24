ad6114a2418594c6ecf4e169090546d45eca6f8b
user622628252416 <nnew8715@gmail.com>
2024-11-24 16:28:38 +0100

    fmt

--- pumpkin/benches/registry_bench.rs
+++ pumpkin/benches/registry_bench.rs
@@ -1,12 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn get_block_by_id(c: &mut Criterion) {
-    c.bench_function("get block by id using hash: last", |b| b.iter(|| 
-        pumpkin_world::block::block_registry::get_block(black_box("minecraft:pale_hanging_moss"))
-    ));
-    c.bench_function("get block by id using hash: first", |b| b.iter(|| 
-        pumpkin_world::block::block_registry::get_block(black_box("minecraft:air"))
-    ));
+    c.bench_function("get block by id using hash: last", |b| {
+        b.iter(|| {
+            pumpkin_world::block::block_registry::get_block(black_box(
+                "minecraft:pale_hanging_moss",
+            ))
+        })
+    });
+    c.bench_function("get block by id using hash: first", |b| {
+        b.iter(|| pumpkin_world::block::block_registry::get_block(black_box("minecraft:air")))
+    });
 }
 
 criterion_group!(benches, get_block_by_id);
