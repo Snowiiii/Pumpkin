dfc869b91708003aa79c420c72f3a3823feb842b
vyPal <kubik.palacky@gmail.com>
2024-11-24 19:58:55 +0100

    Add example plugin

---
+++ plugins/hello-plugin-source/src/lib.rs
@@ -0,0 +1,19 @@
+use pumpkin_api::*;
+use pumpkin_api_macros::{plugin_method, plugin_impl};
+
+plugin_metadata!("Plugin name", "plugin-id", "1.0.0", &["Author Name"], "Description");
+
+#[plugin_method]
+fn on_load(&mut self, server: &dyn PluginContext) -> Result<(), String> {
+    server.get_logger().info("Plugin loaded!");
+    Ok(())
+}
+
+#[plugin_method]
+fn on_unload(&mut self, server: &dyn PluginContext) -> Result<(), String> {
+    server.get_logger().info("Plugin unloaded!");
+    Ok(())
+}
+
+#[plugin_impl]
+pub struct MyPlugin {}
