9b988374ec43d9134cb3f4dbf4017873846afab3
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2024-11-23 19:49:52 -0800

    Added sound event to entity

--- pumpkin-protocol/src/client/play/c_entity_sound_effect.rs
+++ pumpkin-protocol/src/client/play/c_entity_sound_effect.rs
@@ -1,12 +1,12 @@
 use pumpkin_macros::client_packet;
 use serde::Serialize;
 
-use crate::{SoundCategory, VarInt};
+use crate::{IDOrSoundEvent, SoundCategory, SoundEvent, VarInt};
 
 #[derive(Serialize)]
 #[client_packet("play:sound_entity")]
 pub struct CEntitySoundEffect {
-    sound_event: VarInt,
+    sound_event: IDOrSoundEvent,
     sound_category: VarInt,
     entity_id: VarInt,
     volume: f32,
@@ -16,7 +16,8 @@ pub struct CEntitySoundEffect {
 
 impl CEntitySoundEffect {
     pub fn new(
-        sound_event: VarInt,
+        sound_id: VarInt,
+        sound_event: Option<SoundEvent>,
         sound_category: SoundCategory,
         entity_id: VarInt,
         volume: f32,
@@ -24,7 +25,10 @@ impl CEntitySoundEffect {
         seed: f64,
     ) -> Self {
         Self {
-            sound_event: VarInt(sound_event.0 + 1),
+            sound_event: IDOrSoundEvent {
+                id: VarInt(sound_id.0 + 1),
+                sound_event,
+            },
             sound_category: VarInt(sound_category as i32),
             entity_id,
             volume,
