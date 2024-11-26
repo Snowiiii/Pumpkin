cbfca647c9960b816ec09a89dff6553b2f61f033
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2024-11-25 20:00:04 -0800

    Moved ClientPackets

--- pumpkin-protocol/src/client/play/c_entity_sound_effect.rs
+++ pumpkin-protocol/src/client/play/c_entity_sound_effect.rs
@@ -2,26 +2,6 @@ use pumpkin_macros::client_packet;
 
 use crate::{ClientPacket, IDOrSoundEvent, SoundCategory, SoundEvent, VarInt};
 
-impl ClientPacket for CEntitySoundEffect {
-    fn write(&self, bytebuf: &mut crate::bytebuf::ByteBuffer) {
-        bytebuf.put_var_int(&self.sound_event.id);
-        if self.sound_event.id.0 == 0 {
-            if let Some(test) = &self.sound_event.sound_event {
-                bytebuf.put_string(&test.sound_name);
-
-                bytebuf.put_option(&test.range, |p, v| {
-                    p.put_f32(*v);
-                });
-            }
-        }
-        bytebuf.put_var_int(&self.sound_category);
-        bytebuf.put_var_int(&self.entity_id);
-        bytebuf.put_f32(self.volume);
-        bytebuf.put_f32(self.pitch);
-        bytebuf.put_f64(self.seed);
-    }
-}
-
 #[client_packet("play:sound_entity")]
 pub struct CEntitySoundEffect {
     sound_event: IDOrSoundEvent,
@@ -54,5 +34,25 @@ impl CEntitySoundEffect {
             pitch,
             seed,
         }
+    }
+}
+
+impl ClientPacket for CEntitySoundEffect {
+    fn write(&self, bytebuf: &mut crate::bytebuf::ByteBuffer) {
+        bytebuf.put_var_int(&self.sound_event.id);
+        if self.sound_event.id.0 == 0 {
+            if let Some(test) = &self.sound_event.sound_event {
+                bytebuf.put_string(&test.sound_name);
+
+                bytebuf.put_option(&test.range, |p, v| {
+                    p.put_f32(*v);
+                });
+            }
+        }
+        bytebuf.put_var_int(&self.sound_category);
+        bytebuf.put_var_int(&self.entity_id);
+        bytebuf.put_f32(self.volume);
+        bytebuf.put_f32(self.pitch);
+        bytebuf.put_f64(self.seed);
     }
 }
--- pumpkin-protocol/src/client/play/c_sound_effect.rs
+++ pumpkin-protocol/src/client/play/c_sound_effect.rs
@@ -2,28 +2,6 @@ use pumpkin_macros::client_packet;
 
 use crate::{ClientPacket, IDOrSoundEvent, SoundCategory, SoundEvent, VarInt};
 
-impl ClientPacket for CSoundEffect {
-    fn write(&self, bytebuf: &mut crate::bytebuf::ByteBuffer) {
-        bytebuf.put_var_int(&self.sound_event.id);
-        if self.sound_event.id.0 == 0 {
-            if let Some(test) = &self.sound_event.sound_event {
-                bytebuf.put_string(&test.sound_name);
-
-                bytebuf.put_option(&test.range, |p, v| {
-                    p.put_f32(*v);
-                });
-            }
-        }
-        bytebuf.put_var_int(&self.sound_category);
-        bytebuf.put_i32(self.effect_position_x);
-        bytebuf.put_i32(self.effect_position_y);
-        bytebuf.put_i32(self.effect_position_z);
-        bytebuf.put_f32(self.volume);
-        bytebuf.put_f32(self.pitch);
-        bytebuf.put_f64(self.seed);
-    }
-}
-
 #[client_packet("play:sound")]
 pub struct CSoundEffect {
     sound_event: IDOrSoundEvent,
@@ -62,5 +40,27 @@ impl CSoundEffect {
             pitch,
             seed,
         }
+    }
+}
+
+impl ClientPacket for CSoundEffect {
+    fn write(&self, bytebuf: &mut crate::bytebuf::ByteBuffer) {
+        bytebuf.put_var_int(&self.sound_event.id);
+        if self.sound_event.id.0 == 0 {
+            if let Some(test) = &self.sound_event.sound_event {
+                bytebuf.put_string(&test.sound_name);
+
+                bytebuf.put_option(&test.range, |p, v| {
+                    p.put_f32(*v);
+                });
+            }
+        }
+        bytebuf.put_var_int(&self.sound_category);
+        bytebuf.put_i32(self.effect_position_x);
+        bytebuf.put_i32(self.effect_position_y);
+        bytebuf.put_i32(self.effect_position_z);
+        bytebuf.put_f32(self.volume);
+        bytebuf.put_f32(self.pitch);
+        bytebuf.put_f64(self.seed);
     }
 }
