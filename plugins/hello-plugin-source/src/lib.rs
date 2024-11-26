4eb45b54d4e5493956368323cee351ac2dc32990
vyPal <kubik.palacky@gmail.com>
2024-11-26 19:02:31 +0100

    Update example

--- plugins/hello-plugin-source/src/lib.rs
+++ plugins/hello-plugin-source/src/lib.rs
@@ -13,9 +13,15 @@ fn on_unload(&mut self, server: &dyn PluginContext) -> Result<(), String> {
     Ok(())
 }
 
-#[plugin_event]
-fn on_player_join(&mut self, server: &dyn PluginContext, event: &mut dyn PlayerConnectionEvent) -> Result<(), String> {
+#[plugin_event(blocking = true, priority = Highest)]
+fn on_player_join(&mut self, server: &dyn PluginContext, event: &mut PlayerConnection) -> Result<(), String> {
     server.get_logger().info(format!("Player {} joined the game", event.get_player().gameprofile.name).as_str());
+    Ok(())
+}
+
+#[plugin_event]
+fn on_player_leave(&mut self, server: &dyn PluginContext, event: &mut PlayerConnection) -> Result<(), String> {
+    server.get_logger().info(format!("Player {} left the game", event.get_player().gameprofile.name).as_str());
     Ok(())
 }
 
