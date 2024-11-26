0cc693e4b26228049c34a48984f2f9c40201f100
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-11-26 21:28:59 +0100

    Add Server Links (#307)
    * Add Server Links
    
    * Use config phase
    
    ---------

--- pumpkin-protocol/src/client/config/mod.rs
+++ pumpkin-protocol/src/client/config/mod.rs
@@ -5,6 +5,7 @@ mod c_finish_config;
 mod c_known_packs;
 mod c_plugin_message;
 mod c_registry_data;
+mod c_server_links;
 mod c_store_cookie;
 mod c_transfer;
 
@@ -15,5 +16,6 @@ pub use c_finish_config::*;
 pub use c_known_packs::*;
 pub use c_plugin_message::*;
 pub use c_registry_data::*;
+pub use c_server_links::*;
 pub use c_store_cookie::*;
 pub use c_transfer::*;
