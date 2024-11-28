8863457eb573a64bf4dd51e02c4d9329a3de9bb8
Hongas <ndhiqvocuc@gmail.com>
2024-11-28 14:14:29 +0700

    Removes serde_inline

--- pumpkin-config/src/translation.rs
+++ pumpkin-config/src/translation.rs
@@ -1,18 +1,13 @@
 use std::path::PathBuf;
 
 use serde::{Deserialize, Serialize};
-use serde_inline_default::serde_inline_default;
 
-#[serde_inline_default]
 #[derive(Deserialize, Serialize)]
 #[serde(default)]
 pub struct TranslationConfig {
-    #[serde_inline_default(true)]
     pub enabled: bool,
-    #[serde_inline_default(true)]
     pub client_translations: bool,
     // Reminder to update every new version, or until a better method is found
-    #[serde_inline_default(None)]
     pub translation_file_path: Option<PathBuf>,
 }
 
