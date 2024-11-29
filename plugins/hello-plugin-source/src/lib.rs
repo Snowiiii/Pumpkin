69c932eaf19339578aba1164cd68716920f81110
vyPal <kubik.palacky@gmail.com>
2024-11-29 14:46:13 +0100

    Update example

--- plugins/hello-plugin-source/src/lib.rs
+++ plugins/hello-plugin-source/src/lib.rs
@@ -1,6 +1,8 @@
-use pumpkin::plugin::*;
-use pumpkin_api_macros::{plugin_impl, plugin_method, plugin_event};
 use pumpkin::entity::player::Player;
+use pumpkin::plugin::*;
+use pumpkin_api_macros::{plugin_event, plugin_impl, plugin_method};
+use pumpkin_core::text::TextComponent;
+use pumpkin_core::GameMode;
 
 #[plugin_method]
 fn on_load(&mut self, server: &dyn PluginContext) -> Result<(), String> {
@@ -15,15 +17,31 @@ fn on_unload(&mut self, server: &dyn PluginContext) -> Result<(), String> {
 }
 
 #[plugin_event(blocking = true, priority = Highest)]
-fn on_player_join(&mut self, server: &dyn PluginContext, player: &Player) -> Result<bool, String> {
-    server.get_logger().info(format!("Player {} joined the game", player.gameprofile.name).as_str());
+async fn on_player_join(
+    &mut self,
+    server: &dyn PluginContext,
+    player: &Player,
+) -> Result<bool, String> {
+    server
+        .get_logger()
+        .info(format!("Player {} joined the game", player.gameprofile.name).as_str());
+    /// TODO: Calling any method that involves sending packets to the player will cause the client to crash
+    //let _ = player.send_system_message(&TextComponent::text("Hello, world!")).await;
+    //player.set_gamemode(GameMode::Creative).await;
+    //player.kill().await;
     // Returning true will block any other plugins from receiving this event
     Ok(true)
 }
 
 #[plugin_event]
-fn on_player_leave(&mut self, server: &dyn PluginContext, player: &Player) -> Result<bool, String> {
-    server.get_logger().info(format!("Player {} left the game", player.gameprofile.name).as_str());
+async fn on_player_leave(
+    &mut self,
+    server: &dyn PluginContext,
+    player: &Player,
+) -> Result<bool, String> {
+    server
+        .get_logger()
+        .info(format!("Player {} left the game", player.gameprofile.name).as_str());
     Ok(false)
 }
 
