91c5e5c022dc6c21fc5a05edab423af40d18b779
neelesh.poli2006@gmail.com <72574589+neeleshpoli@users.noreply.github.com>
2024-11-29 16:03:36 -0600

    Fmt

--- pumpkin/src/entity/player.rs
+++ pumpkin/src/entity/player.rs
@@ -24,8 +24,8 @@ use pumpkin_core::{
 use pumpkin_entity::{entity_type::EntityType, EntityId};
 use pumpkin_inventory::player::PlayerInventory;
 use pumpkin_macros::sound;
-use pumpkin_protocol::server::play::{SCookieResponse as SPCookieResponse, SPlayPingRequest};
 use pumpkin_protocol::server::play::{SClickContainer, SKeepAlive};
+use pumpkin_protocol::server::play::{SCookieResponse as SPCookieResponse, SPlayPingRequest};
 use pumpkin_protocol::{
     bytebuf::packet_id::Packet,
     client::play::{
@@ -767,7 +767,8 @@ impl Player {
                     .await;
             }
             SPlayPingRequest::PACKET_ID => {
-                self.handle_play_ping_request(SPlayPingRequest::read(bytebuf)?).await;
+                self.handle_play_ping_request(SPlayPingRequest::read(bytebuf)?)
+                    .await;
             }
             SClickContainer::PACKET_ID => {
                 self.handle_click_container(server, SClickContainer::read(bytebuf)?)
