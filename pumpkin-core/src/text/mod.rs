529208ce7d8590cc7a7e96fa4d5e0c658a2e3b84
Commandcracker <49335821+Commandcracker@users.noreply.github.com>
2024-11-30 21:21:09 +0100

    Handle extra in console

--- pumpkin-core/src/text/mod.rs
+++ pumpkin-core/src/text/mod.rs
@@ -90,6 +90,9 @@ impl<'a> TextComponent<'a> {
                 text = format!("\x1b]8;;{}\x1b\\{}\x1b]8;;\x1b\\", url, text).to_string()
             }
         }
+        for child in self.extra {
+            text = text + &*child.to_pretty_console();
+        }
         text
     }
 }
