70986ac57e8ff998d4ab8e943ffbb0c2fdd21302
kralverde <github@email.kralverde.dev>
2024-12-10 17:25:24 -0500

    use time with random seed

--- pumpkin-core/src/random/mod.rs
+++ pumpkin-core/src/random/mod.rs
@@ -1,4 +1,7 @@
-use std::sync::atomic::{AtomicU64, Ordering};
+use std::{
+    sync::atomic::{AtomicU64, Ordering},
+    time,
+};
 
 use legacy_rand::{LegacyRand, LegacySplitter};
 use xoroshiro128::{Xoroshiro, XoroshiroSplitter};
@@ -10,12 +13,21 @@ pub mod xoroshiro128;
 static SEED_UNIQUIFIER: AtomicU64 = AtomicU64::new(8682522807148012u64);
 
 pub fn get_seed() -> u64 {
-    SEED_UNIQUIFIER
+    let seed = SEED_UNIQUIFIER
         .fetch_update(Ordering::Relaxed, Ordering::Relaxed, |val| {
             Some(val.wrapping_mul(1181783497276652981u64))
         })
         // We always return Some, so there will always be an Ok result
+        .unwrap();
+
+    let nanos = time::SystemTime::now()
+        .duration_since(time::SystemTime::UNIX_EPOCH)
         .unwrap()
+        .as_nanos();
+
+    let nano_upper = (nanos >> 8) as u64;
+    let nano_lower = nanos as u64;
+    seed ^ nano_upper ^ nano_lower
 }
 
 pub enum RandomGenerator {
@@ -199,7 +211,7 @@ pub trait RandomDeriverImpl {
     fn split_pos(&self, x: i32, y: i32, z: i32) -> impl RandomImpl;
 }
 
-fn hash_block_pos(x: i32, y: i32, z: i32) -> i64 {
+pub fn hash_block_pos(x: i32, y: i32, z: i32) -> i64 {
     let l = (x.wrapping_mul(3129871) as i64) ^ ((z as i64).wrapping_mul(116129781i64)) ^ (y as i64);
     let l = l
         .wrapping_mul(l)
