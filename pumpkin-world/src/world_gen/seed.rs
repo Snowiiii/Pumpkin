1e2bfad4f939ee8156ee646c671d58a71c596d34
kralverde <80051564+kralverde@users.noreply.github.com>
2024-12-13 08:10:07 -0500

    Parse seeds same as java (#391)
    * parse seeds same as java
    
    * use time with random seed

--- pumpkin-world/src/world_gen/seed.rs
+++ pumpkin-world/src/world_gen/seed.rs
@@ -1,15 +1,20 @@
-use std::hash::{DefaultHasher, Hash, Hasher};
+use pumpkin_core::random::{get_seed, java_string_hash, legacy_rand::LegacyRand, RandomImpl};
 
 #[derive(Clone, Copy)]
-pub struct Seed(pub i64);
+pub struct Seed(pub u64);
 
 impl From<&str> for Seed {
     fn from(value: &str) -> Self {
-        // TODO replace with a deterministic hasher (the same as vanilla?)
-        let mut hasher = DefaultHasher::new();
-        value.hash(&mut hasher);
+        let trimmed = value.trim();
+        let value = if !trimmed.is_empty() {
+            let i64_value = trimmed
+                .parse::<i64>()
+                .unwrap_or_else(|_| java_string_hash(trimmed) as i64);
+            Some(i64_value as u64)
+        } else {
+            None
+        };
 
-        // TODO use cast_signed once the feature is stabilized.
-        Self(hasher.finish() as i64)
+        Seed(value.unwrap_or_else(|| LegacyRand::from_seed(get_seed()).next_i64() as u64))
     }
 }
