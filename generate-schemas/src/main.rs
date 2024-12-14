af1a3eb349775f046ffb7637dabd96b5fd200745
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-14 11:18:30 -0600

    refactor(generate-schemas): also generate a schema for the advanced config

--- generate-schemas/src/main.rs
+++ generate-schemas/src/main.rs
@@ -1,12 +1,23 @@
-use pumpkin_config::BasicConfiguration;
+use pumpkin_config::{AdvancedConfiguration, BasicConfiguration};
 use schemars::schema_for;
 
 fn main() {
-    println!("Hello, world!");
-    let schema = schema_for!(BasicConfiguration);
+    let basic_schema = schema_for!(BasicConfiguration);
+    let advanced_schema = schema_for!(AdvancedConfiguration);
     std::fs::write(
         "../configuration.schema.json",
-        serde_json::to_string_pretty(&schema).unwrap(),
+        serde_json::to_string_pretty(&basic_schema)
+            .map_err(|err| eprintln!("Couldn't generate basic schema: {}", err))
+            .unwrap_or("{\"error\": \"Couldn't generate basic schema\"}".to_string()),
     )
+    .map_err(|err| eprintln!("Couldn't write basic schema: {}", err))
+    .unwrap();
+    std::fs::write(
+        "../features.schema.json",
+        serde_json::to_string_pretty(&advanced_schema)
+            .map_err(|err| eprintln!("Couldn't generate basic schema: {}", err))
+            .unwrap_or("{\"error\": \"Couldn't generate basic schema\"}".to_string()),
+    )
+    .map_err(|err| eprintln!("Couldn't write advanced schema: {}", err))
     .unwrap();
 }
