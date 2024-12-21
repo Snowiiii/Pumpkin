fd667901cdeb16ace017bc5a584df4b14f083274
Alexander Medvedev <lilalexmed@proton.me>
2024-12-21 17:09:27 +0100

    add codecs folder to git

---
+++ pumpkin-protocol/src/codec/mod.rs
@@ -0,0 +1,27 @@
+use std::num::NonZeroUsize;
+
+use bytes::{Buf, BufMut};
+use thiserror::Error;
+
+pub mod bit_set;
+pub mod identifier;
+pub mod var_int;
+pub mod var_long;
+
+pub trait Codec<T> {
+    const MAX_SIZE: NonZeroUsize;
+
+    fn written_size(&self) -> usize;
+
+    fn encode(&self, write: &mut impl BufMut);
+
+    fn decode(read: &mut impl Buf) -> Result<T, DecodeError>;
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Error)]
+pub enum DecodeError {
+    #[error("Incomplete VarInt decode")]
+    Incomplete,
+    #[error("VarInt is too large")]
+    TooLarge,
+}
