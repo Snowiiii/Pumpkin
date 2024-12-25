035fd59b645f26fa78141c4811b62eadedfd8aa0
OfficialKris <37947442+OfficialKris@users.noreply.github.com>
2024-12-25 08:26:36 -0800

    Fix deadlock in standard function

--- pumpkin/src/block/blocks/mod.rs
+++ pumpkin/src/block/blocks/mod.rs
@@ -42,15 +42,16 @@ pub async fn standard_open_container<C: Container + Default + 'static>(
     window_type: WindowType,
 ) {
     let entity_id = player.entity_id();
-    let mut open_containers = server.open_containers.write().await;
     // If container exists, add player to container, otherwise create new container
     if let Some(container_id) = server.get_container_id(location, block.clone()).await {
+        let mut open_containers = server.open_containers.write().await;
         log::debug!("Using previous standard container ID: {}", container_id);
         if let Some(container) = open_containers.get_mut(&u64::from(container_id)) {
             container.add_player(entity_id);
             player.open_container.store(Some(container_id.into()));
         }
     } else {
+        let mut open_containers = server.open_containers.write().await;
         let new_id = server.new_container_id();
         log::debug!("Creating new standard container ID: {}", new_id);
         let open_container =
