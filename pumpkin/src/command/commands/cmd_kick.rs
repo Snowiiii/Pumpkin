4b0abbb270435bc7bce07aed4dfe2f8c5e4025c7
Alexander Medvedev <lilalexmed@proton.me>
2024-12-27 16:59:11 +0100

    add to readme

--- pumpkin/src/command/commands/cmd_kick.rs
+++ pumpkin/src/command/commands/cmd_kick.rs
@@ -49,6 +49,7 @@ impl CommandExecutor for KickExecutor {
     }
 }
 
+// TODO: Permission
 pub fn init_command_tree<'a>() -> CommandTree<'a> {
     CommandTree::new(NAMES, DESCRIPTION)
         .with_child(argument(ARG_TARGET, &PlayersArgumentConsumer).execute(&KickExecutor))
